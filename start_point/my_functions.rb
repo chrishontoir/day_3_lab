079a83e9e4a993ce4ab67ac48f8e59ae373016c0
user <user@users-MacBook-Pro.local>
2018-11-28 15:50:43 +0000

    All tests completed successfully.

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -8,3 +8,24 @@ def add_array_lengths(array1,array2)
   end
   return length
 end
+
+def sum_array(numbers)
+  total = 0
+  for number in numbers
+    total += number
+  end
+  return total
+end
+
+def find_item(array,item)
+  for house in array
+    if house == item
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
